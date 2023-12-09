ca86b685367dcee2ffea0bf8275705425323a688
fawoyera <femiawoyera@gmail.com>
2023-12-09 15:39:37 +0000

    Add custom functions

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the length of a sring
+ * @s: string input
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
